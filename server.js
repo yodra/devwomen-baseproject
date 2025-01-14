c8f1cb2a0c81f59d135f66abb330f19162c64597
yodralopez <yodralopez@gmail.com>
2025-01-14 09:34:52 +0000

    endpoint example

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
@@ -10,8 +11,20 @@ let movies = [
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(cors());
 
 // API
+app.get('/movies', (req, res) => {
+
+    if (Math.random() > 0.5) {
+        res.status(500).send('Error en el servidor');
+        return;
+    }
+
+    setTimeout(() => {
+        res.json(movies);
+    }, 2000);
+});
 
 app.listen(3000, () => {
     console.log('Ready on port 3000!');
